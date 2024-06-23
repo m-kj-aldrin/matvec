895a7072fbf010407eb4d59c50228d11dfce8681
MKJ <mkj.aldrin@icloud.com>
2024-06-23 22:32:42 +0200

    yep

--- src/mat.js
+++ src/mat.js
@@ -4,211 +4,291 @@ import { Vec2, Vec3 } from "./vec.js";
 /**@typedef {[[number,number,number,number],[number,number,number,number],[number,number,number,number],[number,number,number,number]]} M4 */
 
 export class Mat3 {
-    /**@type {M3} */
-    #m = [
-        [1, 0, 0],
-        [0, 1, 0],
-        [0, 0, 1],
-    ];
+  /**@type {M3} */
+  #m = [
+    [1, 0, 0],
+    [0, 1, 0],
+    [0, 0, 1],
+  ];
 
-    /**
-     * @param {number} x
-     * @param {number} y
-     */
-    static fromTranslation(x, y) {
-        return new Mat3([
-            [1, 0, x],
-            [0, 1, y],
-            [0, 0, 1],
-        ]);
-    }
+  /**
+   * @param {number} x
+   * @param {number} y
+   */
+  static fromTranslation(x, y) {
+    return new Mat3([
+      [1, 0, x],
+      [0, 1, y],
+      [0, 0, 1],
+    ]);
+  }
 
-    /**
-     * @param {number} angle
-     */
-    static fromRotation(angle) {
-        let c = Math.cos(angle);
-        let s = Math.sin(angle);
+  /**
+   * @param {number} angle
+   */
+  static fromRotation(angle) {
+    let c = Math.cos(angle);
+    let s = Math.sin(angle);
 
-        let m = new Mat3([
-            [c, -s, 0],
-            [s, c, 0],
-            [0, 0, 1],
-        ]);
+    let m = new Mat3([
+      [c, -s, 0],
+      [s, c, 0],
+      [0, 0, 1],
+    ]);
 
-        return m;
-    }
+    return m;
+  }
 
-    /**
-     * @param  {...Mat3} m
-     */
-    static mult(...m) {
-        let result = m.slice(1).reduce((matrixA, cm) => {
-            let result = [];
-            let matrixB = cm.#m;
-            for (let rowIndex = 0; rowIndex < 3; rowIndex++) {
-                let row = [];
-                let row_i = matrixA[rowIndex];
-                for (let colIndex = 0; colIndex < 3; colIndex++) {
-                    let c = 0;
-                    for (let k = 0; k < 3; k++) {
-                        c += row_i[k] * matrixB[k][colIndex];
-                    }
-                    row.push(c);
-                }
-                result.push(row);
-            }
-            return result;
-        }, m[0].#m);
-
-        let newMatrix = new Mat3(result);
-        return newMatrix;
-    }
-
-    /**@param {M3} [m] */
-    constructor(m) {
-        if (Array.isArray(m)) {
-            this.#m = [...m];
+  /**
+   * @param  {...Mat3} m
+   */
+  static mult(...m) {
+    let result = m.slice(1).reduce((matrixA, cm) => {
+      let result = [];
+      let matrixB = cm.#m;
+      for (let rowIndex = 0; rowIndex < 3; rowIndex++) {
+        let row = [];
+        let row_i = matrixA[rowIndex];
+        for (let colIndex = 0; colIndex < 3; colIndex++) {
+          let c = 0;
+          for (let k = 0; k < 3; k++) {
+            c += row_i[k] * matrixB[k][colIndex];
+          }
+          row.push(c);
         }
-    }
+        result.push(row);
+      }
+      return result;
+    }, m[0].#m);
 
-    get m() {
-        return [...this.#m];
-    }
+    let newMatrix = new Mat3(result);
+    return newMatrix;
+  }
 
-    toString() {
-        let s = "";
-        this.#m.forEach((r) => {
-            s += r.toString() + "\n";
-        });
-        return s;
+  /**@param {M3} [m] */
+  constructor(m) {
+    if (Array.isArray(m)) {
+      this.#m = [...m];
     }
+  }
 
-    /**
-     * @param {number} x
-     * @param {number} y
-     */
-    translate(x, y) {
-        let m = Mat3.fromTranslation(x, y);
-        let _m = Mat3.mult(this, m);
-        this.#m = _m.#m;
-        return this;
-    }
+  get m() {
+    return [...this.#m];
+  }
 
-    /**
-     * @param {Mat3} mv
-     */
-    mult(mv) {
-        return Mat3.mult(this, mv);
-    }
+  toString() {
+    let s = "";
+    this.#m.forEach((r) => {
+      s += r.toString() + "\n";
+    });
+    return s;
+  }
+
+  /**
+   * @param {number} x
+   * @param {number} y
+   */
+  translate(x, y) {
+    let m = Mat3.fromTranslation(x, y);
+    let _m = Mat3.mult(this, m);
+    this.#m = _m.#m;
+    return this;
+  }
+
+  /**
+   * @param {Mat3} mv
+   */
+  mult(mv) {
+    return Mat3.mult(this, mv);
+  }
 }
 
 export class Mat4 {
-    /**@type {M4} */
-    #m = [
-        [1, 0, 0, 0],
-        [0, 1, 0, 0],
-        [0, 0, 1, 0],
-        [0, 0, 0, 1],
-    ];
+  /**@type {M4} */
+  #m = [
+    [1, 0, 0, 0],
+    [0, 1, 0, 0],
+    [0, 0, 1, 0],
+    [0, 0, 0, 1],
+  ];
 
-    /**
-     * @param  {...Mat4} m
-     */
-    static mult(...m) {
-        let result = m.slice(1).reduce((matrixA, cm) => {
-            let result = [];
-            let matrixB = cm.#m;
-            for (let rowIndex = 0; rowIndex < 4; rowIndex++) {
-                let row = [];
-                let row_i = matrixA[rowIndex];
-                for (let colIndex = 0; colIndex < 4; colIndex++) {
-                    let c = 0;
-                    for (let k = 0; k < 4; k++) {
-                        c += row_i[k] * matrixB[k][colIndex];
-                    }
-                    row.push(c);
-                }
-                result.push(row);
-            }
-            return result;
-        }, m[0].#m);
-
-        let newMatrix = new Mat4(result);
-        return newMatrix;
-    }
-
-    /**
-     *
-     * @param {number} x
-     * @param {number} y
-     * @param {number} z
-     */
-    static fromTranslation(x, y, z) {
-        return new Mat4([
-            [1, 0, 0, x],
-            [0, 1, 0, y],
-            [0, 0, 1, z],
-            [0, 0, 0, 1],
-        ]);
-    }
-
-    /**
-     *
-     * @param {number} angle
-     * @param {Vec3} axis
-     */
-    static fromRotation(angle, axis) {
-        let c = Math.cos(angle);
-        let s = Math.sin(angle);
-        let t = 1 - c;
-
-        if (axis.length <= 0) {
-            return new Mat4();
+  /**
+   * @param  {...Mat4} m
+   */
+  static mult(...m) {
+    let result = m.slice(1).reduce((matrixA, cm) => {
+      let result = [];
+      let matrixB = cm.#m;
+      for (let rowIndex = 0; rowIndex < 4; rowIndex++) {
+        let row = [];
+        let row_i = matrixA[rowIndex];
+        for (let colIndex = 0; colIndex < 4; colIndex++) {
+          let c = 0;
+          for (let k = 0; k < 4; k++) {
+            c += row_i[k] * matrixB[k][colIndex];
+          }
+          row.push(c);
         }
+        result.push(row);
+      }
+      return result;
+    }, m[0].#m);
 
-        let { x, y, z } = Vec3.normalize(axis);
+    let newMatrix = new Mat4(result);
+    return newMatrix;
+  }
 
-        let newMat = new Mat4([
-            [x * x * t + c, y * x * t + z * s, z * x * t - y * s, 0],
-            [x * y * t - z * s, y * y * t + c, z * y * t + x * s, 0],
-            [x * z * t + y * s, y * z * t - x * s, z * z * t + c, 0],
-            [0, 0, 0, 1],
-        ]);
+  /**
+   *
+   * @param {number} x
+   * @param {number} y
+   * @param {number} z
+   */
+  static fromTranslation(x, y, z) {
+    return new Mat4([
+      [1, 0, 0, x],
+      [0, 1, 0, y],
+      [0, 0, 1, z],
+      [0, 0, 0, 1],
+    ]);
+  }
 
-        return newMat;
+  /**
+   *
+   * @param {number} angle
+   * @param {Vec3} axis
+   */
+  static fromRotation(angle, axis) {
+    let c = Math.cos(angle);
+    let s = Math.sin(angle);
+    let t = 1 - c;
 
-        // out[0] = x * x * t + c;
-        // out[1] = y * x * t + z * s;
-        // out[2] = z * x * t - y * s;
-        // out[3] = 0;
-        // out[4] = x * y * t - z * s;
-        // out[5] = y * y * t + c;
-        // out[6] = z * y * t + x * s;
-        // out[7] = 0;
-        // out[8] = x * z * t + y * s;
-        // out[9] = y * z * t - x * s;
-        // out[10] = z * z * t + c;
-        // out[11] = 0;
-        // out[12] = 0;
-        // out[13] = 0;
-        // out[14] = 0;
-        // out[15] = 1;
+    if (axis.length <= 0) {
+      return new Mat4();
     }
 
-    /**@param {M4} [m] */
-    constructor(m) {
-        if (Array.isArray(m)) {
-            this.#m = [...m];
-        }
-    }
+    let { x, y, z } = Vec3.normalize(axis);
 
-    get m() {
-        return this.#m;
-    }
+    let newMat = new Mat4([
+      [x * x * t + c, y * x * t + z * s, z * x * t - y * s, 0],
+      [x * y * t - z * s, y * y * t + c, z * y * t + x * s, 0],
+      [x * z * t + y * s, y * z * t - x * s, z * z * t + c, 0],
+      [0, 0, 0, 1],
+    ]);
 
-    /**@param {Mat4} m */
-    mult(m) {
-        return Mat4.mult(this, m);
+    return newMat;
+  }
+
+  /**
+   * @param {number} x
+   * @param {number} y
+   * @param {number} z
+   */
+  static fromScale(x, y, z) {
+    let m = new Mat4([
+      [x, 0, 0, 0],
+      [0, y, 0, 0],
+      [0, 0, z, 0],
+      [0, 0, 0, 1],
+    ]);
+    return m;
+  }
+
+  /**
+   *
+   * @param {Vec3} eye
+   * @param {Vec3} target
+   * @param {Vec3} up
+   */
+  static fromView(eye, target, up) {
+    let F = Vec3.sub(eye, target).normalize();
+    let R = Vec3.cross(up, F).normalize();
+    let U = Vec3.cross(F, R);
+
+    let m = new Mat4([
+      [R.x, R.y, R.z, -Vec3.dot(R, eye)],
+      [U.x, U.y, U.z, -Vec3.dot(U, eye)],
+      [F.x, F.y, F.z, -Vec3.dot(F, eye)],
+      [0, 0, 0, 1],
+    ]);
+
+    return m;
+  }
+
+  /**@param {M4} [m] */
+  constructor(m) {
+    if (Array.isArray(m)) {
+      this.#m = [...m];
     }
+  }
+
+  get m() {
+    return this.#m;
+  }
+
+  /**
+   * @param {Mat4} m
+   */
+  mult(m) {
+    return Mat4.mult(this, m);
+  }
+
+  /**
+   *
+   * @param {number} x
+   * @param {number} y
+   * @param {number} z
+   */
+  translate(x, y, z) {
+    let translation = Mat4.fromTranslation(x, y, z);
+    // this.mult(translation)
+    let m = translation.mult(this);
+    this.#m = m.#m;
+    return this;
+  }
+
+  /**
+   *
+   * @param {number} angle
+   * @param {Vec3} axis
+   */
+  rotate(angle, axis) {
+    let rotation = Mat4.fromRotation(angle, axis);
+    let m = rotation.mult(this);
+    this.#m = m.#m;
+    return this;
+  }
+
+  /**
+   *
+   * @param {number} x
+   * @param {number} y
+   * @param {number} z
+   *
+   */
+  scale(x, y, z) {
+    let scaling = Mat4.fromScale(x, y, z);
+    let m = scaling.mult(this);
+    this.#m = m.#m;
+    return this;
+  }
+
+  /**
+   * @param {number} [precision]
+   */
+  toString(precision = 2) {
+    let [r0, r1, r2, r3] = this.#m;
+    return `${r0[0].toFixed(precision)} ,\t${r0[1].toFixed(precision)} ,\t${r0[2].toFixed(
+      precision
+    )} ,\t${r0[3].toFixed(precision)}
+${r1[0].toFixed(precision)} ,\t${r1[1].toFixed(precision)} ,\t${r1[2].toFixed(
+      precision
+    )} ,\t${r1[3].toFixed(precision)}
+${r2[0].toFixed(precision)} ,\t${r2[1].toFixed(precision)} ,\t${r2[2].toFixed(
+      precision
+    )} ,\t${r2[3].toFixed(precision)}
+${r3[0].toFixed(precision)} ,\t${r3[1].toFixed(precision)} ,\t${r3[2].toFixed(
+      precision
+    )} ,\t${r3[3].toFixed(precision)}`;
+  }
 }
--- src/vec.js
+++ src/vec.js
@@ -1,89 +1,186 @@
 import { Mat4 } from "./mat.js";
 
 export class Vec2 {
-    #x = 0;
-    #y = 0;
+  #x = 0;
+  #y = 0;
 
-    /**
-     * @param {number} [x]
-     * @param {number} [y]
-     */
-    constructor(x, y) {
-        this.#x = x ?? 0;
-        this.#y = y ?? 0;
-    }
+  /**
+   * @param {number} [x]
+   * @param {number} [y]
+   */
+  constructor(x, y) {
+    this.#x = x ?? 0;
+    this.#y = y ?? 0;
+  }
 
-    set x(v) {
-        this.#x = v;
-    }
+  set x(v) {
+    this.#x = v;
+  }
 
-    set y(v) {
-        this.#y = v;
-    }
+  set y(v) {
+    this.#y = v;
+  }
 
-    get x() {
-        return this.#x;
-    }
+  get x() {
+    return this.#x;
+  }
 
-    get y() {
-        return this.#y;
-    }
+  get y() {
+    return this.#y;
+  }
 
-    get length() {
-        return Math.hypot(this.#x, this.#y);
-    }
+  get length() {
+    return Math.hypot(this.#x, this.#y);
+  }
 }
 
 export class Vec3 {
-    #x = 0;
-    #y = 0;
-    #z = 0;
+  #x = 0;
+  #y = 0;
+  #z = 0;
 
-    /**
-     * @param {number} x
-     * @param {number} y
-     * @param {number} z
-     */
-    constructor(x, y, z) {
-        this.#x = x;
-        this.#y = y;
-        this.#z = z;
-    }
+  /**
+   * @param {number} x
+   * @param {number} y
+   * @param {number} z
+   */
+  constructor(x, y, z) {
+    this.#x = x;
+    this.#y = y;
+    this.#z = z;
+  }
 
-    /**@param {Vec3} v */
-    static normalize(v) {
-        let length = v.length;
-        v.#x /= length;
-        v.#y /= length;
-        v.#z /= length;
-        return v;
+  /**@param {Vec3} v */
+  static normalize(v) {
+    let length = v.length;
+    if (!length) {
+      return v;
     }
+    v.#x /= length;
+    v.#y /= length;
+    v.#z /= length;
+    return v;
+  }
 
-    get x() {
-        return this.#x;
+  normalize() {
+    let v = this;
+    let length = v.length;
+    if (!length) {
+      return this;
     }
-    get y() {
-        return this.#y;
-    }
-    get z() {
-        return this.#z;
-    }
+    v.#x /= length;
+    v.#y /= length;
+    v.#z /= length;
+    return v;
+  }
 
-    get length() {
-        return Math.hypot(this.#x, this.#y, this.#z);
-    }
+  get x() {
+    return this.#x;
+  }
+  get y() {
+    return this.#y;
+  }
+  get z() {
+    return this.#z;
+  }
 
-    /**@param {Mat4} m */
-    transform(m) {
-        let [r0, r1, r2, r3] = m.m;
+  get length() {
+    return Math.hypot(this.#x, this.#y, this.#z);
+  }
 
-        let x = this.#x * r0[0] + this.#y * r0[1] + this.#z * r0[2] + r0[3];
-        let y = this.#x * r1[0] + this.#y * r1[1] + this.#z * r1[2] + r1[3];
-        let z = this.#x * r2[0] + this.#y * r2[1] + this.#z * r2[2] + r2[3];
+  /**
+   * @param {Vec3} v0
+   * @param {Vec3} v1
+   */
+  static sub(v0, v1) {
+    let x = v0.#x - v1.#x;
+    let y = v0.#y - v1.#y;
+    let z = v0.#z - v1.#z;
 
-        this.#x = x;
-        this.#y = y;
-        this.#z = z;
-        return this;
-    }
+    let newVec = new Vec3(x, y, z);
+
+    return newVec;
+  }
+
+  /**
+   * @param {Vec3} v0
+   * @param {Vec3} v1
+   */
+  static cross(v0, v1) {
+    // console.log(v0.#x, v0.#y, v0.#z);
+    // console.log(v1.#x, v1.#y, v1.#z);
+
+    let x = v0.#y * v1.#z - v0.#z * v1.#y;
+    // console.log(v0.#y);
+    let y = v0.#z * v1.#x - v0.#x * v1.#z;
+    let z = v0.#x * v1.#y - v0.#y * v1.#x;
+
+    let newVec = new Vec3(x, y, z);
+
+    // console.log(newVec.x, newVec.y, newVec.z);
+
+    return newVec;
+  }
+
+  /**
+   * @param {Vec3} v0
+   * @param {Vec3} v1
+   */
+  static dot(v0, v1) {
+    return v0.#x * v1.#x + v0.#y * v1.#y + v0.#z * v1.#z;
+  }
+
+  /**
+   * @param {Vec3} v
+   * @param {Mat4} m
+   */
+  static transform(v, m, directional = false) {
+    let [r0, r1, r2, r3] = m.m;
+
+    let w = directional ? 0 : 1;
+
+    let x = v.#x * r0[0] + v.#y * r0[1] + v.#z * r0[2] + r0[3] * w;
+    let y = v.#x * r1[0] + v.#y * r1[1] + v.#z * r1[2] + r1[3] * w;
+    let z = v.#x * r2[0] + v.#y * r2[1] + v.#z * r2[2] + r2[3] * w;
+
+    let newVec = new Vec3(x, y, z);
+
+    return newVec;
+  }
+
+  /**@param {Mat4} m */
+  transform(m, directional = false) {
+    let [r0, r1, r2, r3] = m.m;
+
+    let w = directional ? 0 : 1;
+
+    let x = this.#x * r0[0] + this.#y * r0[1] + this.#z * r0[2] + r0[3] * w;
+    let y = this.#x * r1[0] + this.#y * r1[1] + this.#z * r1[2] + r1[3] * w;
+    let z = this.#x * r2[0] + this.#y * r2[1] + this.#z * r2[2] + r2[3] * w;
+
+    this.#x = x;
+    this.#y = y;
+    this.#z = z;
+    return this;
+  }
+
+  /**
+   * @param {Vec3} v
+   */
+  distance(v) {
+    let f = Vec3.sub(v, this);
+    return f.length;
+  }
+
+  clone() {
+    let clone = new Vec3(this.#x, this.#y, this.#z);
+    return clone;
+  }
+
+  /**@param {number} [precision=2] */
+  toString(precision = 2) {
+    return `[ ${this.#x.toFixed(precision)} , ${this.#y.toFixed(precision)} , ${this.#z.toFixed(
+      precision
+    )} ]`;
+  }
 }
